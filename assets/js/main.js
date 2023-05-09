f211e408b26e2aa1b9840b9e88a5bbc94d581d76
Romina <rominchuela@gmail.com>
2023-05-09 00:33:23 -0400

    ya

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@
 // id="cajaRemitente" /// aqui se le hace clic pa sacarle la negrita
 
 function quitarNegrita(){
-    document.getElementById('remitenteNuevo').style.fontWeight = "500";
-    document.getElementById('nuevoActivo').style.fontWeight = "400";
+    document.getElementById('remitenteNuevo').classList.remove('fw-bold'); // = "500";
+    document.getElementById('nuevoActivo').classList.remove('fw-bold');
     document.getElementById('nuevoActivo').innerHTML = "Visto";
 }
 
@@ -17,3 +17,30 @@ function quitarNegrita(){
 var cajaRemitente = document.getElementById('cajaRemitente');
 
 cajaRemitente.addEventListener('click', quitarNegrita);
+
+
+////////////// FUNCION QUE APARECE EL CHAT Y DESAPARECE LOS CONTACTOS
+
+// id="chats"
+// id="contactoMobile"
+// id="verChat"
+
+
+function apareceChat(){
+    document.getElementById('chats').classList.remove('d-none'); 
+    document.getElementById('contactoMobile').classList.add('d-none');  
+}
+function apareceContacto(){
+    document.getElementById('contactoMobile').classList.remove('d-none');
+    document.getElementById('chats').classList.add('d-none'); 
+}
+
+
+var verChat = document.getElementById('verChat');
+verChat.addEventListener('click', apareceChat);
+
+var verContacto = document.getElementById('verContacto');
+verContacto.addEventListener('click',apareceContacto);
+
+var contactoMobile = document.getElementById('contactoMobile');
+contactoMobile.addEventListener('click',apareceChat);
